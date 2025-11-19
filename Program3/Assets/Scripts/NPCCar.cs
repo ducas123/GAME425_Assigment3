56cc59936453ea88288e3aa16d8322f11e703393
phartung-04 <phartung@arizona.edu>
2025-11-18 17:06:56 -0700

    Starting Catmull-Rom

--- Program3/Assets/Scripts/NPCCar.cs
+++ Program3/Assets/Scripts/NPCCar.cs
@@ -3,6 +3,9 @@
 // Sample code file
 
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class NPCCar : Car
 {
@@ -11,5 +14,28 @@ public class NPCCar : Car
         moveInput =  0f;
         steerInput = 0f;
         brakeInput = false;
+    }
+
+    //Catmull-Rom set-up
+    public float position;
+    Vector3 b0;
+    Vector3 b1;
+    Vector3 b2;
+    Vector3 b3;
+    
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
     }
 }
