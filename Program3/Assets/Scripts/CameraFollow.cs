2d2925c3fc9b94b4b1b4cdcf8271500bb2d9a8d6
gallodj04 <gallodj04@arizona.edu>
2025-12-02 16:49:12 -0700

    Car Stuff?
    Plz work

--- Program3/Assets/Scripts/CameraFollow.cs
+++ Program3/Assets/Scripts/CameraFollow.cs
@@ -10,14 +10,27 @@ public class CameraFollow : MonoBehaviour
 {
     public GameObject player;
     public Vector3 offset = new Vector3(-1.73f, 5.48f, -14.86f);
+    public float springConstant = 1f;
+    public float dampConstant = 1f;
+    private Vector3 velocity;
+    private Vector3 actualPosition;
 
     private void Start()
     {
-        this.transform.rotation = player.transform.rotation;
-        this.transform.position = player.transform.position + offset;
+        actualPosition = transform.position + offset;
     }
     private void FixedUpdate()
     {
-        this.transform.position = player.transform.position + offset;
+        Vector3 idealPosition =
+            player.transform.position - player.transform.forward * offset.x + player.transform.up * offset.y;
+        Vector3 displacement = actualPosition - idealPosition;
+
+        Vector3 springAccel = (-springConstant * displacement) - (dampConstant * velocity);
+        velocity += springAccel * Time.deltaTime;
+        actualPosition += velocity * Time.deltaTime;
+        
+        this.transform.position = actualPosition;
+        transform.LookAt(player.transform.position);
+        
     }
 }
